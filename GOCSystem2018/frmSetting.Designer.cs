0d51dc180250de49637d34ad9450e925979a3c5c
Efraim Basilio <basilio.efraim@gmail.com>
2019-02-15 14:41:02 +0800

    Set up the Generic Settings

---
+++ GOCSystem2018/frmSetting.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace GOCSystem2018
+{
+    partial class frmSetting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtDSN = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSaveConfiguration = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(-13, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 141;
+            this.button1.Text = "Report Viewer";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtDSN
+            // 
+            this.txtDSN.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDSN.Location = new System.Drawing.Point(113, 11);
+            this.txtDSN.Name = "txtDSN";
+            this.txtDSN.Size = new System.Drawing.Size(200, 23);
+            this.txtDSN.TabIndex = 129;
+            this.txtDSN.Text = "AcquaintanceSTI2016";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(-29, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 16);
+            this.label4.TabIndex = 140;
+            this.label4.Text = "Data Source Name : *";
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.GhostWhite;
+            this.btnClose.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnClose.Location = new System.Drawing.Point(214, 188);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(99, 28);
+            this.btnClose.TabIndex = 135;
+            this.btnClose.Text = "Clos&e";
+            this.btnClose.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnClose.UseVisualStyleBackColor = false;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(113, 78);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(200, 23);
+            this.txtName.TabIndex = 131;
+            this.txtName.Text = "db_mrmssti2017";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHost.Location = new System.Drawing.Point(113, 44);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(200, 23);
+            this.txtHost.TabIndex = 130;
+            this.txtHost.Text = "rjain";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(-16, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 16);
+            this.label3.TabIndex = 137;
+            this.label3.Text = "Database Name : *";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(52, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 136;
+            this.label2.Text = "Host : *";
+            // 
+            // btnSaveConfiguration
+            // 
+            this.btnSaveConfiguration.BackColor = System.Drawing.Color.GhostWhite;
+            this.btnSaveConfiguration.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveConfiguration.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnSaveConfiguration.Location = new System.Drawing.Point(-13, 188);
+            this.btnSaveConfiguration.Name = "btnSaveConfiguration";
+            this.btnSaveConfiguration.Size = new System.Drawing.Size(99, 28);
+            this.btnSaveConfiguration.TabIndex = 134;
+            this.btnSaveConfiguration.Text = "Save";
+            this.btnSaveConfiguration.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnSaveConfiguration.UseVisualStyleBackColor = false;
+            this.btnSaveConfiguration.Click += new System.EventHandler(this.btnSaveConfiguration_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(113, 149);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(200, 23);
+            this.txtPassword.TabIndex = 133;
+            this.txtPassword.Text = "12345";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUser.Location = new System.Drawing.Point(113, 111);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(200, 23);
+            this.txtUser.TabIndex = 132;
+            this.txtUser.Text = "myuser";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 139;
+            this.label1.Text = "Password :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(16, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 16);
+            this.label5.TabIndex = 138;
+            this.label5.Text = "Username : *";
+            // 
+            // frmSetting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 289);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDSN);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSaveConfiguration);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Name = "frmSetting";
+            this.Text = "frmSetting";
+            this.Load += new System.EventHandler(this.frmSetting_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmSetting_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtDSN;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSaveConfiguration;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ GOCSystem2018/frmSetting.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GOCSystem2018
+{
+    public partial class frmSetting : Form
+    {
+        public frmSetting()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaveConfiguration_Click(object sender, EventArgs e)
+        {
+            if (this.CheckRequiredFields() == true)
+            {
+                //set properties
+                GOCSystem2018.Config.DSN = txtDSN.Text;
+                GOCSystem2018.Config.DB_HOST = txtHost.Text;
+                GOCSystem2018.Config.DB_NAME = txtName.Text;
+                GOCSystem2018.Config.DB_USER = txtUser.Text;
+                GOCSystem2018.Config.DB_PASSWORD = txtPassword.Text;
+
+                //save settings
+                GOCSystem2018.Config.saveSettings();
+
+                if (GOCSystem2018.Config.TestConnection())
+                {
+                    MessageBox.Show("Server database configuration has been successfully updated", "System Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    //if (flag == "Client")
+                    //{
+                    //    frmFrontMain frmFrontMain = new frmFrontMain();
+                    //    frmFrontMain.ShowDialog();
+                    //    this.Dispose();
+                    //    this.Close();
+                    //}
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please fill out all required fields", "System Message", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void frmSetting_Load(object sender, EventArgs e)
+        {
+            if (!(GOCSystem2018.Config.DSN == string.Empty))
+            {
+                this.GetServerSettings();
+            }
+        }
+
+        private void GetServerSettings()
+        {
+            txtDSN.Text = GOCSystem2018.Config.DSN;
+            txtHost.Text = GOCSystem2018.Config.DB_HOST;
+            txtName.Text = GOCSystem2018.Config.DB_NAME;
+            txtUser.Text = GOCSystem2018.Config.DB_USER;
+            txtPassword.Text = GOCSystem2018.Config.DB_PASSWORD;
+        }
+
+        private bool CheckRequiredFields()
+        {
+            if (txtDSN.Text == "")
+            {
+                return false;
+            }
+
+            if (txtHost.Text == "")
+            {
+                return false;
+            }
+
+            if (txtName.Text == "")
+            {
+                return false;
+            }
+
+            if (txtUser.Text == "")
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void frmSetting_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+                this.Dispose();
+            }
+            else if (e.KeyCode == Keys.Enter)
+            {
+                btnSaveConfiguration.PerformClick();
+            }
+        }
+    }
+}
