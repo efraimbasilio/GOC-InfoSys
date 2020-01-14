e9b941b02ec874a0ec611ed64e33db4391d7c66a
Efraim Basilio <basilio.efraim@gmail.com>
2020-01-14 16:43:27 +0800

    update

---
+++ GOCSystem2018/frmNewTuitionFee.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GOCSystem2018
+{
+    public partial class frmNewTuitionFee : Form
+    {
+        Models.TuitionFee tuition = new Models.TuitionFee();
+        List<Models.TuitionFee> listTuition = new List<Models.TuitionFee>();
+
+        Util_RequiredFields util = new Util_RequiredFields();
+
+        public int id;
+
+        public void AddImageDataGrid(DataGridView dgv)
+        {
+            DataGridViewImageColumn dimg = new DataGridViewImageColumn();
+            dimg.Image = Properties.Resources.edit;
+            dimg.HeaderText = "Edit";
+            dimg.ImageLayout = DataGridViewImageCellLayout.Normal;
+            dimg.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            dgv.Columns.Add(dimg);
+
+
+            DataGridViewImageColumn dimgDelete = new DataGridViewImageColumn();
+            dimgDelete.Image = Properties.Resources.delete;
+            dimgDelete.HeaderText = "Delete";
+            dimgDelete.ImageLayout = DataGridViewImageCellLayout.Normal;
+            dimgDelete.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            dgv.Columns.Add(dimgDelete);
+        }
+
+        public void HeaderFix(DataGridView dgv)
+        {
+            #region Header Name
+            dgv.Columns["id"].Visible = false;
+            
+            DataGridViewColumn FillSize = dgv.Columns[1];
+            FillSize.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            #endregion
+        }
+
+        public void Reset()
+        {
+            txtTuitionFeeName.Text = "";
+            txtAmount.Text = "";
+            txtDesc.Text = "";
+            cmbStatus.Text = "";
+
+            //optAmount.Checked = false;
+            //optPercent.Checked = false;
+            btnAdd.Text = "&Add New";
+
+        }
+
+        public frmNewTuitionFee()
+        {
+            InitializeComponent();
+        }
+    }
+}
